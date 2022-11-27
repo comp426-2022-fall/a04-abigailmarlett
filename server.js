41aa21b6964ccf423371ab66add4a2d203a5f3ee
Abigail Marlett <amarlett@email.unc.edu>
2022-11-27 18:58:31 -0500

    outline comments

--- server.js
+++ server.js
@@ -21,3 +21,30 @@ app.get('/app/', (req, res) => {
     res.status(200);
     res.send("200 OK");
 });
+
+//PART 4
+//Endpoint /app/roll/ that returns JSON for a default roll of two six-sided dice one time. 
+//Example output might look like: {"sides":6,"dice":2,"rolls":1,"results":[12]}.
+
+
+
+//PART 5
+//Endpoint /app/roll/ should ALSO accept either JSON or URLEncoded data body for sides, dice, and rolls. 
+//Example URLEncoded string for data body: ?sides=20&dice=4&rolls=3. Example JSON data body: {"sides":20,"dice":4,"rolls":3}. The format of the resulting JSON should look like: {"sides":20,"dice":4,"rolls":3,"results":[19,3,60]}.
+
+//PART 6
+//Endpoint /app/roll/:sides/ that returns JSON for a default number of rolls and dice with whatever number of sides is specified in the parameter. 
+//For example, /app/roll/6/ should return JSON for two six-sided dice, rolled one time, whereas /app/roll/10/ should return JSON for two ten-sided dice, rolled 1 time. The format of the resulting JSON should look like: {"sides":10,"dice":2,"rolls":1,"results":[17]}.
+
+
+//PART 7
+//Endpoint /app/roll/:sides/:dice/ that returns JSON for a default number of rolls with whatever number of sides and dice specified in the parameters. 
+//For example, /app/roll/6/2/ should return JSON for two six-sided dice, rolled one time, whereas /app/roll/10/3/ should return JSON for three ten-sided dice, rolled 1 time. The format of the resulting JSON should look like: {"sides":10,"dice":3,"rolls":1,"results":[27]}.
+
+
+//PART 8
+//Endpoint /app/roll/:sides/:dice/:rolls/ that returns JSON for the specified number of rolls with whatever number of sides and dice specified in the parameters. 
+//For example, /app/roll/6/2/1/ should return JSON for two six-sided dice, rolled one time, whereas /app/roll/10/3/8/ should return JSON for three ten-sided dice, rolled 1 time. The format of the resulting JSON should look like: {"sides":10,"dice":3,"rolls":8,"results":[6,13,30,17,16,27,4,29]}.
+
+//listen at the end
+
