3e295191dcd8944062caabe8767e6f3225876df6
Abigail Marlett <amarlett@email.unc.edu>
2022-11-27 18:53:20 -0500

    200 OK

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ const args = minimist(process.argv.slice(2))
 
 //creating the port and making it 5000 if not specified
 const port = args.port || 5000
+
+app.use(express.urlencoded({extended:true}));
+
+//Check endpoint at /app/ that returns 200 OK.
+app.get('/app/', (req, res) => {
+    res.status(200);
+    res.send("200 OK");
+});
